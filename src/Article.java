3a71dacadb285dc8bb89741f7fb677668d09c413
mhlsf <mahel.sif@gmail.com>
2017-10-19 17:40:23 +0200

    Add global variables, extend categorie

--- src/Article.java
+++ src/Article.java
@@ -2,4 +2,16 @@
  * Created by msif on 19/10/17.
  */
 public abstract class Article {
+
+    private float price;
+
+    public float getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice()
+    {
+        this.price = price;
+    }
 }
---
+++ src/GlobalVariable.java
@@ -0,0 +1,9 @@
+/**
+ * Created by msif on 19/10/17.
+ */
+public interface GlobalVariable {
+
+    public static int UNJOUR = 24;
+    public static int DEUXJOUR = 48;
+    public static int TROISJOUR = 72;
+}

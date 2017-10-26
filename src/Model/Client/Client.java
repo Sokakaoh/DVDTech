e6b1515f8c77ec5030d91e80d2a1a37256133922
Sokakaoh <loanfiey@gmail.com>
2017-10-26 16:59:27 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Article.java
    #	src/Client.java
    #	src/DeAnnée.java
    #	src/Location.java
    #	src/Nouveauté.java
    #	src/PlusAncien.java
    #	src/Récent.java
    #	src/deAnnée.java

--- src/Model/Client/Client.java
+++ src/Model/Client/Client.java
@@ -1,5 +1,8 @@
 package Model.Client;
 
+import Model.Article.Article;
+import Model.Article.Film;
+
 import java.util.ArrayList;
 
 /**
@@ -12,4 +15,74 @@ public class Client {
     private String adresseClient;
     private String mailClient;
     private String telClient;
+
+    public Client(ArrayList<Location>locationsClient, String nomClient, String prenomClient, String adresseClient, String mailClient, String telClient){
+        this.locationsClient = locationsClient;
+        this.nomClient = nomClient;
+        this.prenomClient = prenomClient;
+        this.adresseClient = adresseClient;
+        this.mailClient = mailClient;
+        this.telClient = telClient;
+    }
+
+    public Client(){
+
+    }
+
+    public void faireLocation(Location l){
+        this.locationsClient.add(l);
+    }
+
+    public ArrayList<Location> getLocationsClient() {
+        return locationsClient;
+    }
+
+    public void setLocationsClient(ArrayList<Location> locationsClient) {
+        this.locationsClient = locationsClient;
+    }
+
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    public void setNomClient(String nomClient) {
+        this.nomClient = nomClient;
+    }
+
+    public String getPrenomClient() {
+        return prenomClient;
+    }
+
+    public void setPrenomClient(String prenomClient) {
+        this.prenomClient = prenomClient;
+    }
+
+    public String getAdresseClient() {
+        return adresseClient;
+    }
+
+    public void setAdresseClient(String adresseClient) {
+        this.adresseClient = adresseClient;
+    }
+
+    public String getMailClient() {
+        return mailClient;
+    }
+
+    public void setMailClient(String mailClient) {
+        this.mailClient = mailClient;
+    }
+
+    public String getTelClient() {
+        return telClient;
+    }
+
+    public void setTelClient(String telClient) {
+        this.telClient = telClient;
+    }
+
+    public String toString(){
+        return "Nom du client : " +nomClient + " , prenom du client :" + prenomClient + " , adresse du client : " + adresseClient
+                + " mail du client : " +mailClient + "telephone du client" + telClient;
+    }
 }
